c420657236f6467928a04b11b0677194bb4d04eb
Rodwyn <rodwynmoreno@gmail.com>
2019-10-02 16:30:15 -0500

    Implemented material-ui dependency.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Home from './pages/Home';
+import customTheme from './config/muiTheme';
 
 const App = ({ store }) => (
 	<Provider store={store}>
-		<Router>
-			<div>
-				<Route exact path="/" component={Home} />
-			</div>
-		</Router>
+		<MuiThemeProvider theme={ customTheme }>
+			<Router>
+				<div>
+					<Route exact path="/" component={Home} />
+				</div>
+			</Router>
+		</MuiThemeProvider>
 	</Provider>
 );
 
