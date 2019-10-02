0689a07ff46c5759bafecc79394f36c70c0676c6
Rodwyn <rodwynmoreno@gmail.com>
2019-10-02 16:02:06 -0500

    Revert "Added materialize-ui library."
    This reverts commit ee214a685465c3a4f6be4dde8993113b5704dbbb.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { MuiThemeProvider } from "@material-ui/core/styles";
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Home from './pages/Home';
-import customTeme from "./config/muiTheme";
 
 const App = ({ store }) => (
-	<Provider store={store}>>
-		<MuiThemeProvider theme={ customTheme } >
-			<Router>
-				<div>
-					<Route exact path="/" component={Home} />
-				</div>
-			</Router>
-		</MuiThemeProvider>
+	<Provider store={store}>
+		<Router>
+			<div>
+				<Route exact path="/" component={Home} />
+			</div>
+		</Router>
 	</Provider>
 );
 
