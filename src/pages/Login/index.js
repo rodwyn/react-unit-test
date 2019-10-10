20fcbdeea8650f6dff3825f1a4944b5f16fa7464
Rodwyn <rodwynmoreno@gmail.com>
2019-10-10 15:29:55 -0500

    Added login page styles.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
 
 import { styles } from './styles';
 
@@ -10,7 +11,9 @@ class Login extends Component {
 
     return (
       <div className={ classes.container }>
-        Iniciar Sesión
+        <Card className={ classes.card }>
+          <Typography className={ classes.title } variant="h1">Iniciar Sesión</Typography>
+        </Card>
       </div>
     )
   }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,3 +1,23 @@
 export const styles = {
-  container: {}
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100vh',
+    overflow: 'hidden',
+    width: '100%'
+  },
+  card: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    minWidth: '300px',
+    width: '400px',
+  },
+  title: {
+    marginTop: 12,
+    fontSize: 18,
+    marginBottom: '4px'
+  }
 };
