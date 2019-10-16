cfcbebd48cb5a23ff3223559c7ae78a53b9e381d
Rodwyn <rodwynmoreno@gmail.com>
2019-10-16 14:32:42 -0500

    Added API calls at home page.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 
+import { getProfileData, getProfileRepos } from '../../redux/actions/profile';
+import { getData } from '../../utils/storage';
+import { GITHUB_TOKEN } from '../../consts';
 import LeftContainer from './components/LeftContainer';
 import RightContainer from './components/RightContainer';
 import {styles} from './style';
 
 class Home extends Component {
+	constructor() {
+		super();
+		this.state = { githubToken: null };
+	}
+
+	componentDidMount() {
+		const githubToken = getData(GITHUB_TOKEN);
+
+		this.setState({ githubToken });
+		console.log(this.props);
+	}
 
 	render() {
 		const { classes } = this.props;
 
 		return (
 			<Grid container className={ classes.homeContainer }>
-				<Grid xs={3} className={classes.leftContainer}>
+				<Grid item xs={3} className={classes.leftContainer}>
 					<LeftContainer />
 				</Grid>
-				<Grid xs={9} className={classes.rightContainer}>
+				<Grid item xs={9} className={classes.rightContainer}>
 					<RightContainer />
 				</Grid>
 			</Grid>
@@ -24,4 +40,17 @@ class Home extends Component {
 	}
 }
 
-export default withStyles(styles)(Home);
+const actions = {
+	getProfileData,
+	getProfileRepos
+};
+
+const mapStateToProps = state => {
+	console.log(state);
+	return {}
+}
+
+export default compose(
+	connect(mapStateToProps, actions),
+	withStyles(styles)
+)(Home);
